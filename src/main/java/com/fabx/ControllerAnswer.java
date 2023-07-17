a28330433faef8c6a9e2d75346ce9244ca3335cb
Alexandre Batista <alexandre.dev23@gmail.com>
2023-07-17 17:03:21 -0300

    Pegando paranetro para salvar a resposta e relacionar com a pergunta apos redirect

--- src/main/java/com/fabx/ControllerAnswer.java
+++ src/main/java/com/fabx/ControllerAnswer.java
@@ -19,14 +19,23 @@ public class ControllerAnswer {
 
 	@Inject
 	private ServiceAsk serviceAsk;
-	
+
 	private List<Ask> asks = new ArrayList<Ask>();
-	
+
 	private Integer id;
-	
+
 	@PostConstruct
 	public void init() {
 		asks = serviceAsk.getAsks();
+	}
+
+	public String recebeParametro(Integer idParam) {
+		System.out.println("Id recebido " + idParam);
+
+		Flash flashScope = FacesContext.getCurrentInstance().getExternalContext().getFlash();
+		flashScope.put("id", idParam);
+
+		return "anwser?faces-redirect=true";
 	}
 
 	public List<Ask> getAsks() {
@@ -36,7 +45,15 @@ public class ControllerAnswer {
 	public void setAsks(List<Ask> asks) {
 		this.asks = asks;
 	}
-	
-	
-	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+
 }
